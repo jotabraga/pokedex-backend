7067548bfee5e26d0ac93cadd8d6e535207a81ea
jotabraga <jpmbraga.feg@gmail.com>
2021-08-16 00:33:26 -0300

    Refator names of tests

--- tests/integration/pokemons.test.ts
+++ tests/integration/pokemons.test.ts
@@ -4,7 +4,11 @@ import app from "../../src/app";
 import * as userFactory from "../factories/userFactory";
 import * as pokemonFactory from "../factories/pokemonFactory";
 
-import { startConnection, endConnection, clearDatabase } from "../utils/database";
+import {
+  startConnection,
+  endConnection,
+  clearDatabase,
+} from "../utils/database";
 
 beforeAll(startConnection);
 beforeEach(clearDatabase);
@@ -13,45 +17,63 @@ afterAll(endConnection);
 const test = supertest(app);
 
 describe("GET /pokemons", () => {
-    it("should answer with status 401 invalid authorization", async () => {
-        const missingHeaderAuthorization = await test.get("/pokemons");
-        const missingBearerToken = await test.get("/pokemons").set("Authorization", `Bearer `);
-        const invalidToken = await test.get("/pokemons").set("Authorization", `Bearer invalidToken`);;
-        expect(missingHeaderAuthorization.status).toBe(401);
-        expect(missingBearerToken.status).toBe(401);
-        expect(invalidToken.status).toBe(401);
-    });
+  it("should answer with status 401 for unauthorized", async () => {
+    const getPokemonsNoAuthorizationTry = await test.get("/pokemons");
+    const getPokemonsNoTokenTry = await test
+      .get("/pokemons")
+      .set("Authorization", `Bearer `);
+    const getPokemonsInvalidTokenTry = await test
+      .get("/pokemons")
+      .set("Authorization", `Bearer invalidToken`);
+    expect(getPokemonsNoAuthorizationTry.status).toBe(401);
+    expect(getPokemonsNoTokenTry.status).toBe(401);
+    expect(getPokemonsInvalidTokenTry.status).toBe(401);
+  });
 
-    it("should answer with status 200 and array of pokemons", async () => {
-        const newUser = await userFactory.registerUser();
-        const newSession = await userFactory.createSession(1);
-        const newPokemon = await pokemonFactory.createPokemon();
-        const result = await test.get("/pokemons").set("Authorization", `Bearer ${newSession.token}`);
-        expect(result.status).toBe(200);
-        expect(result.body[0]).toStrictEqual({ ...newPokemon, "inMyPokemons": false });
-    });
+  it("should answer with status 200 and pokemon array for a valid request", async () => {
+    const user = await userFactory.registerUser();
+    const session = await userFactory.createSession(1);
+    const pokemon = await pokemonFactory.createPokemon();
+    const result = await test
+      .get("/pokemons")
+      .set("Authorization", `Bearer ${session.token}`);
+    expect(result.status).toBe(200);
+    expect(result.body[0]).toStrictEqual({ ...pokemon, inMyPokemons: false });
+  });
 });
 
 describe("POST /my-pokemons/:pokemonId/add", () => {
-    it("should answer with status 401 invalid authorization", async () => {
-        const newPokemon = await pokemonFactory.createPokemon();
-        const missingHeaderAuthorization = await test.post(`/my-pokemons/${newPokemon.id}/add`);
-        const missingBearerToken = await test.post(`/my-pokemons/${newPokemon.id}/add`).set("Authorization", `Bearer`);
-        const invalidToken = await test.post(`/my-pokemons/${newPokemon.id}/add`).set("Authorization", `Bearer invalidToken`);
-        expect(missingHeaderAuthorization.status).toBe(401);
-        expect(missingBearerToken.status).toBe(401);
-        expect(invalidToken.status).toBe(401);
-    });
+  it("should answer with status 401 for unauthorized", async () => {
+    const pokemon = await pokemonFactory.createPokemon();
+    const noAuthorizedAddPokemonTry = await test.post(
+      `/my-pokemons/${pokemon.id}/add`
+    );
+    const noTokenAddPokemonTry = await test
+      .post(`/my-pokemons/${pokemon.id}/add`)
+      .set("Authorization", `Bearer`);
+    const invalidTokenAddPokemonTry = await test
+      .post(`/my-pokemons/${pokemon.id}/add`)
+      .set("Authorization", `Bearer invalidToken`);
+    expect(noAuthorizedAddPokemonTry.status).toBe(401);
+    expect(noTokenAddPokemonTry.status).toBe(401);
+    expect(invalidTokenAddPokemonTry.status).toBe(401);
+  });
 });
 
 describe("POST /my-pokemons/:pokemonId/remove", () => {
-    it("should answer with status 401 invalid authorization", async () => {
-        const newPokemon = await pokemonFactory.createPokemon();
-        const missingHeaderAuthorization = await test.post(`/my-pokemons/${newPokemon.id}/remove`);
-        const missingBearerToken = await test.post(`/my-pokemons/${newPokemon.id}/remove`).set("Authorization", `Bearer`);
-        const invalidToken = await test.post(`/my-pokemons/${newPokemon.id}/remove`).set("Authorization", `Bearer invalidToken`);
-        expect(missingHeaderAuthorization.status).toBe(401);
-        expect(missingBearerToken.status).toBe(401);
-        expect(invalidToken.status).toBe(401);
-    });
+  it("should answer with status 401 invalid authorization", async () => {
+    const pokemon = await pokemonFactory.createPokemon();
+    const noAuthorizedRemovePokemonTry = await test.post(
+      `/my-pokemons/${pokemon.id}/remove`
+    );
+    const noTokenRemovePokemonTry = await test
+      .post(`/my-pokemons/${pokemon.id}/remove`)
+      .set("Authorization", `Bearer`);
+    const invalidTokenRemovePokemonTry = await test
+      .post(`/my-pokemons/${pokemon.id}/remove`)
+      .set("Authorization", `Bearer invalidToken`);
+    expect(noAuthorizedRemovePokemonTry.status).toBe(401);
+    expect(noTokenRemovePokemonTry.status).toBe(401);
+    expect(invalidTokenRemovePokemonTry.status).toBe(401);
+  });
 });
